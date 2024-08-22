a8d1d375c2c29d157ac23ed775f61b60375ee559
winter100 <100taeng@gmail.com>
2024-08-22 21:44:26 +0900

    [#4] feat: 기본 헤더 생성

--- src/app/_components/layout/Header.tsx
+++ src/app/_components/layout/Header.tsx
@@ -1,5 +1,10 @@
 const Header = () => {
-  return <div>헤더</div>;
+  return (
+    <nav className='h-14 shadow-sm bg-white p-2 rounded-lg w-full flex justify-between items-center'>
+      <div>Heros..로고</div>
+      <div>메뉴들...</div>
+    </nav>
+  );
 };
 
 export default Header;
