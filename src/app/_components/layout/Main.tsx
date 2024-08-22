024c178edf123d73b1a4ab64648e7b731f3e2711
winter100 <100taeng@gmail.com>
2024-08-22 21:41:53 +0900

    [#4] feat: 기본 메인 생성

--- src/app/_components/layout/Main.tsx
+++ src/app/_components/layout/Main.tsx
@@ -1,5 +1,9 @@
-const MainSection = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+import { ComponentProps } from 'react';
+
+interface MainSectionProps extends ComponentProps<'main'> {}
+
+const MainSection = ({ children, ...props }: MainSectionProps) => {
+  return <main {...props}>{children}</main>;
 };
 
 export default MainSection;
