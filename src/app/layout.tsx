97ce4e3e1b9a49c60dec38eac4fac64358753b6b
winter100 <100taeng@gmail.com>
2024-08-22 21:45:31 +0900

    [#2] feat: 기본 레이아웃 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from './_components/layout/Header';
-import Footer from './_components/layout/Footer';
-import MainSection from './_components/layout/Main';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,11 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className={`${inter.className} border border-red-600`}>
-        <Header />
-        <MainSection>{children}</MainSection>
-        <Footer />
-      </body>
+      <body className={`${inter.className} bg-zinc-100`}>{children}</body>
     </html>
   );
 }
