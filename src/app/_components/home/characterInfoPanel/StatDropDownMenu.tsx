268f15d9d5505895e283c194e58a9d5921ecec1e
winter100 <100taeng@gmail.com>
2024-09-09 07:45:48 +0900

    [#35] feat: 스텟 필터에 dnd기능 추가

--- src/app/_components/home/characterInfoPanel/StatDropDownMenu.tsx
+++ src/app/_components/home/characterInfoPanel/StatDropDownMenu.tsx
@@ -3,11 +3,14 @@ import { useState } from "react";
 
 import Button from "../../common/Button";
 import { useRankStore } from "@/app/_store/useRankStore";
+import { useDrag } from "@/app/_hooks/useDrag/useDrag";
 
 const StatDropDownMenu = () => {
   const [view, isView] = useState(false);
   const rankTitleList = useRankStore((state) => state.rankTitleList);
   const toggleView = useRankStore((state) => state.toggleView);
+  const setDropTitleList = useRankStore((state) => state.setDropTitleList);
+  const { dragEnd, dragEnter, dragOver, dragStart } = useDrag(setDropTitleList);
 
   return (
     <div className="inline-block h-full w-24">
@@ -32,10 +35,15 @@ const StatDropDownMenu = () => {
       <div
         className={`absolute mt-1 flex w-24 flex-col rounded-lg bg-black transition-all duration-300 ease-in-out ${view ? "translate-y-0 opacity-100" : "pointer-events-none translate-y-[10px] opacity-0"} `}
       >
-        {rankTitleList.map((t) => (
+        {rankTitleList.map((t, i) => (
           <button
+            draggable
             key={t.stat_name}
             onClick={() => toggleView(t.stat_name)}
+            onDragStart={(e) => dragStart(e, i)}
+            onDragOver={dragOver}
+            onDragEnter={(e) => dragEnter(e, i)}
+            onDragEnd={(e) => dragEnd(e)}
             className={`h-8 hover:bg-zinc-700 ${t.isView === true ? "text-blue-300" : "text-red-300"}`}
           >
             {t.stat_name}
