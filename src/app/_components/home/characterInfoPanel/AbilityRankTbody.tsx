08ef1fe0d0ad80a479710e54b76ff48fcc0c3b40
winter100 <100taeng@gmail.com>
2024-09-09 18:25:32 +0900

    [#35] 테이블 수정 및 리셋 추가, 반응형 추가

--- src/app/_components/home/characterInfoPanel/AbilityRankTbody.tsx
+++ src/app/_components/home/characterInfoPanel/AbilityRankTbody.tsx
@@ -4,27 +4,25 @@ import { v4 as uuidv4 } from "uuid";
 import { useCharacterStore } from "@/app/_store/characterStore";
 import { useRankStore } from "@/app/_store/useRankStore";
 import { filterCharacters } from "./utils/filterCharacters";
-import { fillArrayToEight } from "./utils/fillArrayToEight";
 
 const AbilityRankTbody = () => {
   const characters = useCharacterStore((state) => state.characters);
   const selectedTitleList = useRankStore((state) => state.rankTitleList);
   const filteredCharacters = filterCharacters(characters, selectedTitleList);
 
-  const fillArray = fillArrayToEight(filteredCharacters);
   return (
-    <tbody className="flex h-full w-full flex-1 flex-col rounded-lg bg-zinc-800">
-      {fillArray?.map((c) => (
+    <tbody className="grid h-full w-full grid-rows-8 rounded-lg bg-zinc-800">
+      {filteredCharacters.map((c) => (
         <tr
           key={uuidv4()}
-          className={`flex w-full flex-1 items-center justify-center rounded-lg hover:cursor-pointer hover:bg-zinc-800 hover:text-blue-300`}
+          className={`flex w-full grid-cols-9 items-center justify-center rounded-lg lg:grid ${c?.name?.length > 1 ? "border-b border-gray-400 hover:cursor-pointer hover:text-blue-300" : "border-none"} hover:bg-zinc-800`}
         >
-          {c?.map((item) => (
+          {c?.info.map((i) => (
             <td
               className="flex h-full flex-1 items-center justify-center"
-              key={item?.stat_name + item?.stat_value}
+              key={i?.stat_name + i?.stat_value}
             >
-              {item?.stat_value}
+              {i?.stat_value}
             </td>
           ))}
         </tr>
--- src/app/_components/home/characterInfoPanel/utils/filterCharacters.ts
+++ src/app/_components/home/characterInfoPanel/utils/filterCharacters.ts
@@ -4,28 +4,29 @@ export const filterCharacters = (
   characters: MergedCharacter[],
   selectedTitleList: { stat_name: string; isView: boolean }[],
 ) => {
-  const filterdData = characters.map((c) => {
-    const filterdInfo = c.info
-      .filter((infoItem) => {
-        return selectedTitleList.some(
-          (selectedItem) =>
-            selectedItem.stat_name.includes(infoItem.stat_name) &&
-            selectedItem.isView === true,
-        );
-      })
-      .sort((a, b) => {
-        const indexA = selectedTitleList.findIndex(
-          (selectedItem) => selectedItem.stat_name === a.stat_name,
-        );
-        const indexB = selectedTitleList.findIndex(
-          (selectedItem) => selectedItem.stat_name === b.stat_name,
-        );
-        return indexA - indexB;
-      });
+  const filterdData = characters
+    .map((c) => {
+      const filterdInfo = c?.info
+        .filter((infoItem) => {
+          return selectedTitleList.some(
+            (selectedItem) =>
+              selectedItem.stat_name.includes(infoItem.stat_name) &&
+              selectedItem.isView === true,
+          );
+        })
+        .sort((a, b) => {
+          const indexA = selectedTitleList.findIndex(
+            (selectedItem) => selectedItem.stat_name === a.stat_name,
+          );
+          const indexB = selectedTitleList.findIndex(
+            (selectedItem) => selectedItem.stat_name === b.stat_name,
+          );
+          return indexA - indexB;
+        });
 
-    return filterdInfo;
-  });
-  // .flat();
+      return [{ name: filterdInfo[0]?.stat_value, info: filterdInfo }];
+    })
+    .flat();
 
   return filterdData;
 };
