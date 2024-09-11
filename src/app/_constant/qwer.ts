504f013e16387e9ec372ff0c7403c7ce05e37ef6
winter100 <100taeng@gmail.com>
2024-09-11 22:30:53 +0900

    1

---
+++ src/app/_constant/qwer.ts
@@ -0,0 +1,284 @@
+export const c1 = [
+  {
+    name: "홍길동2",
+    info: [
+      {
+        stat_name: "이름",
+        stat_value: "홍길동2",
+      },
+      {
+        stat_name: "직업",
+        stat_value: "",
+      },
+      {
+        stat_name: "길드",
+        stat_value: "",
+      },
+      {
+        stat_name: "레벨",
+        stat_value: "",
+      },
+      {
+        stat_name: "공격력",
+        stat_value: "24345",
+      },
+      {
+        stat_name: "마법공격력",
+        stat_value: "26254",
+      },
+      {
+        stat_name: "방어력",
+        stat_value: "14722",
+      },
+      {
+        stat_name: "힘",
+        stat_value: "1964",
+      },
+      {
+        stat_name: "민첩",
+        stat_value: "1612",
+      },
+      {
+        stat_name: "지능",
+        stat_value: "3369",
+      },
+      {
+        stat_name: "의지",
+        stat_value: "1569",
+      },
+      {
+        stat_name: "행운",
+        stat_value: "100",
+      },
+      {
+        stat_name: "최대 생명력",
+        stat_value: "9629",
+      },
+      {
+        stat_name: "최대 스태미나",
+        stat_value: "225",
+      },
+      {
+        stat_name: "공격속도",
+        stat_value: "84",
+      },
+      {
+        stat_name: "추가피해",
+        stat_value: "3700",
+      },
+      {
+        stat_name: "크리티컬",
+        stat_value: "120",
+      },
+      {
+        stat_name: "크리티컬 피해량",
+        stat_value: "195",
+      },
+      {
+        stat_name: "크리티컬 저항",
+        stat_value: "119",
+      },
+      {
+        stat_name: "밸런스",
+        stat_value: "88",
+      },
+      {
+        stat_name: "해제",
+        stat_value: "10300",
+      },
+      {
+        stat_name: "대항력",
+        stat_value: "220",
+      },
+    ],
+  },
+  {
+    name: "김길동",
+    info: [
+      {
+        stat_name: "이름",
+        stat_value: "김길동",
+      },
+      {
+        stat_name: "직업",
+        stat_value: "",
+      },
+      {
+        stat_name: "길드",
+        stat_value: "",
+      },
+      {
+        stat_name: "레벨",
+        stat_value: "",
+      },
+      {
+        stat_name: "공격력",
+        stat_value: "234345",
+      },
+      {
+        stat_name: "마법공격력",
+        stat_value: "26254",
+      },
+      {
+        stat_name: "방어력",
+        stat_value: "134722",
+      },
+      {
+        stat_name: "힘",
+        stat_value: "1964",
+      },
+      {
+        stat_name: "민첩",
+        stat_value: "1612",
+      },
+      {
+        stat_name: "지능",
+        stat_value: "3369",
+      },
+      {
+        stat_name: "의지",
+        stat_value: "1569",
+      },
+      {
+        stat_name: "행운",
+        stat_value: "100",
+      },
+      {
+        stat_name: "최대 생명력",
+        stat_value: "9629",
+      },
+      {
+        stat_name: "최대 스태미나",
+        stat_value: "225",
+      },
+      {
+        stat_name: "공격속도",
+        stat_value: "84",
+      },
+      {
+        stat_name: "추가피해",
+        stat_value: "37200",
+      },
+      {
+        stat_name: "크리티컬",
+        stat_value: "1210",
+      },
+      {
+        stat_name: "크리티컬 피해량",
+        stat_value: "195",
+      },
+      {
+        stat_name: "크리티컬 저항",
+        stat_value: "119",
+      },
+      {
+        stat_name: "밸런스",
+        stat_value: "88",
+      },
+      {
+        stat_name: "해제",
+        stat_value: "102300",
+      },
+      {
+        stat_name: "대항력",
+        stat_value: "2220",
+      },
+    ],
+  },
+];
+export const dummyBasic = {
+  character_name: "최아쿠",
+  character_date_create: null,
+  character_date_last_login: "2024-01-12T16:45:06Z",
+  character_date_last_logout: "2024-01-12T16:47:24Z",
+  character_class_name: "이비",
+  character_gender: "F",
+  character_exp: 2163961,
+  character_level: 92,
+  cairde_name: "",
+  title_count: null,
+  id_title_count: null,
+  total_title_count: null,
+  title_stat: [],
+  skill_awakening: [],
+  dress_point: {
+    total_point: 0,
+    avatar_point: 0,
+    back_point: 0,
+    tail_point: 0,
+    object_point: 0,
+  },
+};
+
+export const dummystat = {
+  stat: [
+    {
+      stat_name: "공격력",
+      stat_value: "24345",
+    },
+    {
+      stat_name: "마법공격력",
+      stat_value: "26254",
+    },
+    {
+      stat_name: "방어력",
+      stat_value: "14722",
+    },
+    {
+      stat_name: "힘",
+      stat_value: "1964",
+    },
+    {
+      stat_name: "민첩",
+      stat_value: "1612",
+    },
+    {
+      stat_name: "지능",
+      stat_value: "3369",
+    },
+    {
+      stat_name: "의지",
+      stat_value: "1569",
+    },
+    {
+      stat_name: "행운",
+      stat_value: "100",
+    },
+    {
+      stat_name: "최대 생명력",
+      stat_value: "9629",
+    },
+    {
+      stat_name: "최대 스태미나",
+      stat_value: "225",
+    },
+    {
+      stat_name: "공격속도",
+      stat_value: "84",
+    },
+    {
+      stat_name: "추가피해",
+      stat_value: "3700",
+    },
+    {
+      stat_name: "크리티컬",
+      stat_value: "120",
+    },
+    {
+      stat_name: "크리티컬 피해량",
+      stat_value: "195",
+    },
+    {
+      stat_name: "크리티컬 저항",
+      stat_value: "119",
+    },
+    {
+      stat_name: "밸런스",
+      stat_value: "88",
+    },
+  ],
+};
+
+export const dummyguild = {
+  guild_name: null,
+};
