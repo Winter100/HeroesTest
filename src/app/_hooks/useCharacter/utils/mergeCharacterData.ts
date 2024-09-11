77becb57cd0a1e893778123d762f02b565c14103
winter100 <100taeng@gmail.com>
2024-09-11 22:07:27 +0900

    refactor: useCharacter 의 유틸함수 추가

---
+++ src/app/_hooks/useCharacter/utils/mergeCharacterData.ts
@@ -0,0 +1,37 @@
+import { Basic, Guild, MergedCharacter, Stat } from "@/app/_type/characterType";
+
+export const mergeCharacterData = (
+  basic: Basic,
+  stat: Stat[],
+  // guild: Guild = { guild_name: "" },
+  name: string,
+) => {
+  const mergedCharacterData: MergedCharacter = {
+    name: name,
+    info: [
+      {
+        stat_name: "이름",
+        stat_value: basic?.character_name,
+      },
+      {
+        stat_name: "직업",
+        stat_value: basic?.character_class_name,
+      },
+      {
+        stat_name: "길드",
+        stat_value: "",
+      },
+      {
+        stat_name: "레벨",
+        stat_value: "",
+      },
+      {
+        stat_name: "카르제",
+        stat_value: "",
+      },
+      ...stat,
+    ],
+    stat: [...stat],
+  };
+  return mergedCharacterData;
+};
