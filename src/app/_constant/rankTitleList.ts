830881c1f81d071c0b8660803b0513f9b1fd8533
winter100 <100taeng@gmail.com>
2024-09-13 20:17:28 +0900

    [#47] feat: 길드, 카르제, 레벨 정보 추가

--- src/app/_constant/rankTitleList.ts
+++ src/app/_constant/rankTitleList.ts
@@ -8,4 +8,7 @@ export const initialTitleList = [
   { stat_name: "크리티컬", isView: true },
   { stat_name: "밸런스", isView: true },
   { stat_name: "공격속도", isView: false },
+  { stat_name: "길드", isView: false },
+  { stat_name: "카르제", isView: false },
+  { stat_name: "레벨", isView: false },
 ];
